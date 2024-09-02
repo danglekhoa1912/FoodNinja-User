ce2754af6f0e36da3ea27c57842e2781d789234e
dangkhoa <dangkhoa123bn@gmail.com>
2024-09-03 02:42:58 +0700

    FN-6 Login Page

--- FoodNinja/App/FoodNinjaApp.swift
+++ FoodNinja/App/FoodNinjaApp.swift
@@ -9,9 +9,26 @@ import SwiftUI
 
 @main
 struct FoodNinjaApp: App {
+    @State var endSplash = true
+    @ObservedObject private var appState = AppState()
     var body: some Scene {
         WindowGroup {
-            SplashView()
+            VStack {
+                if endSplash {
+                    SplashView()
+                } else if appState.isOnboardingDone {
+                    LoginView()
+                } else {
+                    OnboardingView().environmentObject(appState)
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                    withAnimation {
+                        endSplash = false
+                    }
+                }
+            }
         }
     }
 }
